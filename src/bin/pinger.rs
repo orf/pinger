0968fa4204498b1e6b8582b2d89a0671c5fc9b90
Tom Forbes <tom@tomforb.es>
2020-11-07 23:04:05 +0000

    Remove serde dependency

--- src/bin/pinger.rs
+++ src/bin/pinger.rs
@@ -2,7 +2,7 @@ use pinger::ping;
 
 fn main() {
     let stream = ping("tomforb.es".to_string()).expect("Error pinging");
-    for message in stream {
+    for message in stream.iter().take(10) {
         println!("{:?}", message);
     }
 }
